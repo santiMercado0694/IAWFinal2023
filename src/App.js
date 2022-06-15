1d8a7e4550e52cb5d25fbc42840d8e693cc3b5e9
santiMercado0694 <santimercado06_94@hotmail.com>
2022-06-15 11:02:39 -0300

    instale material UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Product from "./components/Product"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1> Proyecto IAW REACT</h1>
+      <Product/>
     </div>
   );
 }
