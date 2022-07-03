a13c1f0d92214188d5d05ec5841e38391017a08d
santiMercado0694 <santimercado06_94@hotmail.com>
2022-07-03 10:31:45 -0300

    agregue boton para volver a la tienda

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -5,7 +5,6 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
@@ -121,11 +120,6 @@ export default function SignInSide() {
                 Iniciar sesion
               </Button>
               <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Olvido su contraseña?
-                  </Link>
-                </Grid>
                 <Grid item>
                   <RouteLink to="/signup" >
                     {"Todavia no tiene una cuenta? Registrese"}
