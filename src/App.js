6f81302bbd983fdd25a5cd7f7123768a0b86a53a
santiMercado0694 <santimercado06_94@hotmail.com>
2024-02-23 16:03:27 -0300

    agregue mostrar notificacion dentro de la app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState} from "react";
 import {Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from "./components/layouts/Navbar";
@@ -14,11 +14,40 @@ import { actionTypes} from "./reducer";
 import { useStateValue} from "./StateProvider"
 import { auth} from "./firebase";
 import OneSignal from 'react-onesignal';
+import {ToastContainer, toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
 const [{user}, dispatch] = useStateValue();
+const [hasDisplayedToast, setHasDisplayedToast] = useState(false);
 
+useEffect(() => {
+  OneSignal.init({ appId: 'cbb828bb-a3d0-4d28-b9b8-7093d3efeae6' 
+  });
+},[]);
+
+useEffect(() => {
+  const eventListener = (event) => {
+    const notification = event?.notification;
+    if (notification && notification.title && notification.body && !hasDisplayedToast) {
+      toast.info(`${notification.title}: ${notification.body}`);
+      setHasDisplayedToast(true);
+
+      // Restablecer hasDisplayedToast después de 5 segundos
+      setTimeout(() => {
+        setHasDisplayedToast(false);
+      }, 3000);
+    }
+  };
+
+  OneSignal.Notifications.addEventListener('foregroundWillDisplay', eventListener);
+
+  // Limpia el event listener cuando el componente se desmonta
+  return () => {
+    OneSignal.Notifications.removeEventListener('foregroundWillDisplay', eventListener);
+  };
+}, [hasDisplayedToast]);
 
 useEffect(() => {
   auth.onAuthStateChanged((authUser) =>{
@@ -28,7 +57,6 @@ useEffect(() => {
         type : actionTypes.SET_USER,
         user : authUser,
       })
-      OneSignal.init({ appId: 'cbb828bb-a3d0-4d28-b9b8-7093d3efeae6' });
     }
   })
 },[dispatch])
@@ -36,6 +64,7 @@ useEffect(() => {
   return (
      <Router>
       <div className="App">
+      <ToastContainer /> 
        <Navbar />
                <Switch>
                      <Route exact path="/" component={ProductContainer} />         
