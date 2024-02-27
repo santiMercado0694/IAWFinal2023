7a52b9cb5068b5885422fd9fc5a718c23a1b45e5
santiMercado0694 <santimercado06_94@hotmail.com>
2024-02-27 10:48:58 -0300

    cambio de prueba

--- src/components/layouts/Slider.js
+++ src/components/layouts/Slider.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Carousel } from 'react-bootstrap';
 import gamer1 from '../../assets/Gamer1.webp';
-import gamer2 from '../../assets/Gamer2.webp';
+//import gamer2 from '../../assets/Gamer2.webp';
 import placa from '../../assets/placa.webp';
 import silla from '../../assets/silla.webp';
 import pc from '../../assets/armadas.webp';
@@ -10,7 +10,7 @@ import notebooks from '../../assets/notebooks.webp';
 const CarouselContainer = () => {
     useEffect(() => {
         const preloadImages = async () => {
-            await Promise.all([gamer1, gamer2, placa, silla, pc, notebooks].map(async (imageSrc) => {
+            await Promise.all([gamer1, placa, silla, pc, notebooks].map(async (imageSrc) => {
                 const img = new Image();
                 img.src = imageSrc;
                 await img.decode();
@@ -22,7 +22,7 @@ const CarouselContainer = () => {
     return (
         <div className="Carousel">
             <Carousel>
-                {[gamer1, gamer2, placa, silla, notebooks, pc].map((src, index) => (
+                {[gamer1, placa, silla, notebooks, pc].map((src, index) => (
                     <Carousel.Item key={index}>
                         <img className="d-block w-100" src={src} alt={`Slide ${index + 1}`} loading="lazy" />
                     </Carousel.Item>
