79fc2f35390f50985fb45e3ab1827559a37484b6
santiMercado0694 <santimercado06_94@hotmail.com>
2024-03-07 11:33:03 -0300

    volvi atras en la config del serviceWorker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,54 +1,56 @@
 /* eslint-disable no-restricted-globals */
 import { clientsClaim } from "workbox-core";
+import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate, NetworkFirst } from "workbox-strategies"; // Cambiado a NetworkFirst
-import { ExpirationPlugin } from "workbox-expiration";
+import { StaleWhileRevalidate } from "workbox-strategies";
 import OneSignal from 'react-onesignal';
 
 clientsClaim();
 
-// Precarga y enruta los recursos definidos en __WB_MANIFEST
+// Puedes desactivar el precaching reemplazand esta línea
+// @ts-ignore
 precacheAndRoute(self.__WB_MANIFEST);
+// por esta otra:
+// const desactivarPrecache = self.__WB_MANIFEST;
+// para más info: https://cra.link/PWA
 
-// Expresión regular para verificar la extensión de los archivos
 const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
-
-// Ruta de registro para manejar las solicitudes de navegación
 registerRoute(
+  // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }) => {
+    // If this isn't a navigation, skip.
     if (request.mode !== "navigate") {
       return false;
-    }
+    } // If this is a URL that starts with /\_, skip.
     if (url.pathname.startsWith("/_")) {
       return false;
-    }
+    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
     if (url.pathname.match(fileExtensionRegexp)) {
       return false;
-    }
+    } // Return true to signal that we want to use the handler.
     return true;
   },
   createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
 );
 
-// Ruta de registro para manejar las solicitudes de imágenes en formato WebP
 registerRoute(
-  ({ url }) =>
-    url.origin === self.location.origin && url.pathname.endsWith(".webp"),
-  new NetworkFirst({ // Cambiado a NetworkFirst
-    cacheName: "images",
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 50,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 días
-      }),
-    ],
-  })
-);
+    // Add in any other file extensions or routing criteria as needed.
+    ({ url }) =>
+      url.origin === self.location.origin && url.pathname.endsWith(".png"), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+    new StaleWhileRevalidate({
+      cacheName: "images",
+      plugins: [
+        // Ensure that once this runtime cache reaches a maximum size the
+        // least-recently used images are removed.
+        new ExpirationPlugin({ maxEntries: 50 }),
+      ],
+    })
+  );
 
-// Evento de mensaje para omitir la espera de la activación
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
+    // @ts-ignore
     self.skipWaiting();
   }
 });
@@ -61,16 +63,17 @@ OneSignal.push(function() {
   });
 });
 
-// Manejar las notificaciones push
 self.addEventListener('push', function(event) {
   console.log('Push notification received', event);
 
   var title = 'Push notification';
   var options = {
+    // @ts-ignore
     body: event.data.text(),
     icon: '/icon.png',
     badge: '/badge.png'
   };
 
+  // @ts-ignore
   event.waitUntil(self.registration.showNotification(title, options));
 });
